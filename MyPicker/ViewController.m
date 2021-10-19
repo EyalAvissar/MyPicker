19b3cd3fcbb2438b090fea5c62a636d1c9669c33
EyalAvissar <eyala@inmanage.net>
2021-10-19 20:34:21 +0300

    done

--- MyPicker/ViewController.m
+++ MyPicker/ViewController.m
@@ -16,7 +16,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    array = @[@"Hello", @"Ciao", @"Salut", @"Hola", @"Shalom"];
 }
 
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    self.label.text = array[row];
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return array.count;
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return  1;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return  array[row];
+}
 
 @end
